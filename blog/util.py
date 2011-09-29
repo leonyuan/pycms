e609d80795cb0a7ee7b86f61b40b242da870345d
Leon Yuan <ahaoyuan@gmail.com>
2011-09-29 20:10:29 +0800

    upd blog render path

--- blog/util.py
+++ blog/util.py
@@ -1,8 +1,11 @@
+from os.path import abspath, dirname, join
 from web.contrib.template import render_mako
 
 
+curdir = abspath(dirname(__file__))
+
 render = render_mako(
-            directories=['templates/blog/'],
+            directories=[join(curdir, '../templates/blog/')],
             input_encoding='utf-8',
             output_encoding='utf-8',
          )
