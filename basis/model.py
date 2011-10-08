b2797a314be833bd58452bb4128b436d7e3b9061
Leon Yuan <ahaoyuan@gmail.com>
2011-10-08 11:34:36 +0800

    clean the useless files of article.

--- basis/model.py
+++ basis/model.py
@@ -25,11 +25,6 @@ class Template(Base):
     def __repr__(self):
         return "<Template('%s')>" % (self.name)
 
-'''category_article_asso_table = Table('category_article_asso', Base.metadata,
-    Column('category_id', Integer, ForeignKey('category.id'), primary_key=True),
-    Column('article_id', Integer, ForeignKey('article.id'), primary_key=True)
-)
-'''
 
 class Category(Base):
     __tablename__ = 'category'
@@ -40,36 +35,16 @@ class Category(Base):
     parent_id = Column(Integer, ForeignKey('category.id'))
 
     children = relationship("Category", order_by=id, cascade='delete', backref=backref('parent', remote_side=[id]))
-    #articles = relationship("Article", secondary=category_article_asso_table, backref=backref('categories'))
     model = relationship(Model, backref=backref('categories'))
 
     def __repr__(self):
         return "<Category('%s')>" % (self.name)
 
-'''
-class Article(Base):
-    __tablename__ = 'article'
-    id = Column(Integer, primary_key=True)
-    title = Column(String(64))
-    summary = deferred(Column(Text), group='content')
-    content = deferred(Column(MEDIUMTEXT), group='content')
-    keywords = Column(String(64))
-    thumb = Column(String(100))
-    posted_time = Column(TIMESTAMP, default=utcnow())
-    updated_time = Column(TIMESTAMP, default=utcnow())
-    user_id = Column(Integer, ForeignKey('user.id'))
-
-    user = relationship("User", backref=backref('articles', order_by=id))
-
-    def __unicode__(self):
-        return "<Article('%s')>" % (self.title)
-'''
 
 template_table = Template.__table__
 category_table = Category.__table__
 metadata = Base.metadata
 
-#category_table.extend_existing
 
 if __name__ == "__main__":
     metadata.create_all(engine)
